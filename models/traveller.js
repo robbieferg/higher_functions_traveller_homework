94b674a627d9fdc3544cb58b514b4e8c7918b32e
Robbie Ferguson <robbiejferguson92@gmail.com>
2021-12-15 19:30:43 +0000

    Extensions completed.

--- models/traveller.js
+++ models/traveller.js
@@ -3,28 +3,39 @@ const Traveller = function(journeys) {
 };
 
 Traveller.prototype.getJourneyStartLocations = function() {
-
+  return this.journeys.map((journey) => journey.startLocation);
 };
 
 Traveller.prototype.getJourneyEndLocations = function () {
-
+  return this.journeys.map((journey) => journey.endLocation);
 };
 
 Traveller.prototype.getJourneysByTransport = function (transport) {
-
+  const foundJourneys = this.journeys.filter((journey) => journey.transport === transport);
+  return foundJourneys;
 };
 
 Traveller.prototype.getJourneysByMinDistance = function (minDistance) {
-
+  const foundJourneys = this.journeys.filter((journey) => journey.distance > minDistance);
+  return foundJourneys;
 };
 
 Traveller.prototype.calculateTotalDistanceTravelled = function () {
-
+  let totalDistance = this.journeys.reduce((runningTotal, journey) => runningTotal + journey.distance, 0);
+  return totalDistance;
 };
 
 Traveller.prototype.getUniqueModesOfTransport = function () {
+  const transportList = this.journeys.map((journey) => journey.transport);
+  const uniqueTransports = [];
+  transportList.forEach(function(transport) {
+    if (!uniqueTransports.includes(transport))
+      uniqueTransports.push(transport);
+  })
 
-};
+  return uniqueTransports;
+  
+}
 
 
 module.exports = Traveller;
